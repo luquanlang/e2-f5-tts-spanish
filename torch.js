6e045c4ba6366ad98660202bb60c399f2a6fa12c
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-12-20 18:20:09 +0100

    update
    pin numpy for linux nvidia

--- torch.js
+++ torch.js
@@ -57,7 +57,10 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps"
+        "message": [
+          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall",
+          "uv pip install numpy==1.26.4"
+        ]
       }
     },
     // linux rocm (amd)
